d64047aac0d55f55d09e54a01161aa726b6f769b
Mohammed Mubarak <modymu9@gmail.com>
2023-10-14 14:08:39 +0200

    added test file for storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Tests for the storage model."""
+
+import unittest
+import datetime
+from models.engine.file_storage import FileStorage
+
+
+class TestStorageClass(unittest.TestCase):
+    """Test storage class."""
+    pass
