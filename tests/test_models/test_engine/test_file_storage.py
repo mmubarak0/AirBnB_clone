bed4bb9c0f10b84942be92a191b2ada1ac82aabf
Mohammed Mubarak <modymu9@gmail.com>
2023-10-15 11:09:15 +0200

    added 2 test cases for storage class

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -10,9 +10,29 @@ from models.base_model import BaseModel
 class TestStorageClass(unittest.TestCase):
     """Test storage class."""
     
+    @classmethod
+    def setUpClass(self):
+        """Set test class."""
+        self.storage = FileStorage()
+        self.model_1 = BaseModel()
+        self.model_2 = BaseModel()
+        self.model_3 = BaseModel()
+        self.model_4 = BaseModel()
+        self.model_5 = BaseModel()
+        self.model_6 = BaseModel()
+
+        self.storage.reload()
+
     def test_0(self):
-        """Test Filestorage."""
-        u = FileStorage()
+        """Check if the model key in the __objects when using new."""
+        objects = self.storage.all()
+        self.assertTrue(f"BaseModel.{self.model_1.id}" in objects)
         a = BaseModel()
-        u.new(a)
-        self.assertTrue(type(u.all()) is dict)
+        self.storage.new(a)
+        self.assertTrue(f"BaseModel.{a.id}" in objects)
+
+
+    def test_1(self):
+        """Check the equality of the created model and the model."""
+        objects = self.storage.all()
+        self.assertEqual(objects[f"BaseModel.{self.model_1.id}"], self.model_1)
