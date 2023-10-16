cc81d6eedb61096e86cbd1118f288a00ee5c3d57
Mohammed Mubarak <modymu9@gmail.com>
2023-10-16 20:03:20 +0200

    added tests for user class

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Tests for the base model."""
+"""Tests for the user model."""
 
 import unittest
 import datetime
@@ -8,9 +8,24 @@ from models.user import User
 
 
 class TestUserClass(unittest.TestCase):
-    """Test base class."""
+    """Test user class."""
 
     def test_0(self):
-        """Test amenity."""
+        """Test email attribute."""
         a = User()
-        self.assertEqual(len(a.id), 36)
+        self.assertEqual(type(a.email), str)
+
+    def test_1(self):
+        """Test password attribute."""
+        a = User()
+        self.assertEqual(type(a.password), str)
+
+    def test_2(self):
+        """Test first_name attribute."""
+        a = User()
+        self.assertEqual(type(a.first_name), str)
+
+    def test_3(self):
+        """Test last_name attribute."""
+        a = User()
+        self.assertEqual(type(a.last_name), str)
