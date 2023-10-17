c33cc5a0f90f56eb4acd2a09e97e349434ab7c78
Mohammed Mubarak <modymu9@gmail.com>
2023-10-17 17:58:34 +0200

    added more test for console

--- tests/test_console.py
+++ tests/test_console.py
@@ -37,6 +37,11 @@ class TestBaseClass(unittest.TestCase):
             """
             self.assertNotEqual(help_txt, f.getvalue())
 
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help EOF")
+            help_txt = "End of input stream."
+            self.assertNotEqual(help_txt, f.getvalue())
+
     def test_1(self):
         """Test show method."""
         with patch('sys.stdout', new=StringIO()) as f:
@@ -59,6 +64,16 @@ class TestBaseClass(unittest.TestCase):
             s += r"{[^}]*}"
             self.assertRegex(f.getvalue(), s)
 
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd(f"BaseModel.show(\"{self.model_1.id}\")")
+            # match the [<class name>] part
+            s = r"\[\w*\]\s"
+            # match (<self.id>) part
+            s += r"\([a-z0-9]+-[a-z0-9]+-[a-z0-9]+-[a-zA-Z0-9]+-[a-z0-9]+\)\s"
+            # match <self.__dict__> part
+            s += r"{[^}]*}"
+            self.assertRegex(f.getvalue(), s)
+
     def test_2(self):
         """Test all method."""
         with patch('sys.stdout', new=StringIO()) as f:
@@ -69,3 +84,60 @@ class TestBaseClass(unittest.TestCase):
                 self.assertEqual(f.getvalue()[-2], "]")
             else:
                 self.assertEqual("", f.getvalue())
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd(f"all BaseModel")
+            if os.path.isfile("file.json"):
+                self.assertTrue(len(f.getvalue()) > 0)
+                self.assertEqual(f.getvalue()[0], "[")
+                self.assertEqual(f.getvalue()[-2], "]")
+            else:
+                self.assertEqual("", f.getvalue())
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd(f"BaseModel.all()")
+            if os.path.isfile("file.json"):
+                self.assertTrue(len(f.getvalue()) > 0)
+                self.assertEqual(f.getvalue()[0], "[")
+                self.assertEqual(f.getvalue()[-2], "]")
+            else:
+                self.assertEqual("", f.getvalue())
+
+    def test_3(self):
+        """Test create method."""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+            self.assertEqual(len(f.getvalue()), 37)
+
+    def test_4(self):
+        """Test destroy method."""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd(f"destroy BaseModel {self.model_2.id}")
+            HBNBCommand().onecmd(f"show BaseModel {self.model_2.id}")
+            text = """** no instance found **\n"""
+            self.assertEqual(f.getvalue(), text)
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.model_2 = BaseModel()
+            self.model_2.save()
+            HBNBCommand().onecmd(f"BaseModel.destroy(\"{self.model_2.id}\")")
+            HBNBCommand().onecmd(f"BaseModel.show(\"{self.model_2.id}\")")
+            text = """** no instance found **\n"""
+            self.assertEqual(f.getvalue(), text)
+
+    def test_5(self):
+        """Test update method."""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd(
+                f"update BaseModel {self.model_1.id} name ki2kid"
+            )
+            HBNBCommand().onecmd(f"show BaseModel {self.model_1.id}")
+            self.assertTrue(
+                "name" in f.getvalue() and "ki2kid" in f.getvalue()
+            )
+
+    def test_6(self):
+        """Test count method."""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("BaseModel.count()")
+            self.assertRegex(f.getvalue(), r"\d+")
